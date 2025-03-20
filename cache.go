58215920cff51f7ae80bfc1833d9c87e3ae3114f
sprootik <sprootik89@gmail.com>
2025-03-21 02:50:46 +1000

    fix: del else

--- cache.go
+++ cache.go
@@ -118,7 +118,8 @@ func (c *Cache[K, V]) CacheCapacity() int {
 
 // Add add the element in cache. will return true if a new element was added.
 // If an element was updated or the timeout has expired, returns false.
-func (c *Cache[K, V]) Add(key K, value V) (isNew bool) {
+func (c *Cache[K, V]) Add(key K, value V) bool {
+	isNew := true
 	element := &node[K, V]{key: key, value: value, time: time.Now()}
 
 	c.mu.Lock()
@@ -126,17 +127,15 @@ func (c *Cache[K, V]) Add(key K, value V) (isNew bool) {
 
 	// delete expired element
 	if cEl, ok := c.data[key]; ok {
+		isNew = false
 		if time.Since(cEl.time) <= c.lifeTime {
 			// if key exist, update node value
 			cEl.value = value
 			cEl.time = element.time
 			c.unsafeMoveToTail(cEl)
-			return false
+			return isNew
 		}
 		c.unsafeDelete(cEl)
-
-	} else {
-		isNew = !ok
 	}
 
 	// delete of the latter when the size is exceeded
@@ -145,7 +144,7 @@ func (c *Cache[K, V]) Add(key K, value V) (isNew bool) {
 	}
 
 	c.unsafeAddToTail(element)
-	return
+	return isNew
 }
 
 // Get get from cache by key. Return true if value in cache
